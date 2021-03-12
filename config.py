07827d88ee369060dd24f35a666a8cb8aad8c5f2
2019Python123 <m1270510852@163.com>
2021-03-13 00:48:02 +0800

    配置文件

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+from datetime import timedelta
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = False  # 不自动提交会话到数据库
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite3')
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    SEND_FILE_MAX_AGE_DEFAULT = timedelta(seconds=1)
+    # 邮箱这样配置不会出现535错误
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'm17670415973@163.com'
+    FLASKY_ADMIN = 'm17670415973@163.com'
+    FLASKY_POSTS_PER_PAGE = 8
+    FLASKY_FOLLOWERS_PER_PAGE = 10
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+config ={
+    'config':Config
+}
